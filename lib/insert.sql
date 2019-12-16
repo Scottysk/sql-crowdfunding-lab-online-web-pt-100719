b54aed3345f1f6f209be2b727336086519944468
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-12-16 00:09:44 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,7 +1,7 @@
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('Help me buy a guitar', 'music', 500.00, '2013-06-30', '2013-07-30');
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('My book on SQL', 'books', 20.00, '2013-01-01', '2014-01-01');
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('The next Harry Potter', 'books', 1000.00, '2013-05-30', '2015-05-30');
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Meals on Wheels", "Homeless", 75, "6/1/17", "6/30/17");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('Animal shelter needs dog food', 'charity', 400.00, '2013-03-14', '2013-06-30');
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Alzheimers Run", "Seniors", 100, "6/1/17", "6/30/17");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Counseling Sessions", "Domestic Violence", 65, "7/1/17", "7/31/17");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Wig Making", "Cancer Survivors", 1000, "7/1/17", "7/31/17");
