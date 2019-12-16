485c141db43c27804f6dbd5f91f1b6c6690f504e
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-12-16 00:14:14 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -23,7 +23,7 @@ INSERT INTO users (name, age) VALUES ('Voldemort', 90);
 INSERT INTO users (name, age) VALUES ('Marisa', 24);
 INSERT INTO users (name, age) VALUES ('Swizzle', 4);
 INSERT INTO users (name, age) VALUES ('Sirius', 36);
-INSERT INTO users (name, age) VALUES ("Casey Ulrich", 27);
+INSERT INTO users (name, age) VALUES ('Albus', 113);
 INSERT INTO users (name, age) VALUES ("Taylor Ulrich", 24);
 INSERT INTO users (name, age) VALUES ("Kyle Ulrich", 31);
 INSERT INTO users (name, age) VALUES ("Sean Pinnow", 32);
